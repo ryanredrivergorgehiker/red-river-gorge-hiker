30f5ccad4919054b5f0e36399b8051846f9eea56
ryanredrivergorgehiker <ryan@redrivergorgehiker.com>
2026-08-07 08:31:25 -0400

    Normalize GitHub Pages base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://ryanredrivergorgehiker.github.io',
-  base: '/red-river-gorge-hiker',
+  base: '/red-river-gorge-hiker/',
   output: 'static',
   integrations: [sitemap({ filter: (page) => !page.includes('/assets/images/') })],
   build: { format: 'directory' }
